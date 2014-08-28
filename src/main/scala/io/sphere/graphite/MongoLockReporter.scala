e37cf567d37d2aa674bb13efd21c4a6536a9d655
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2014-08-28 16:55:27 +0200

    More config

--- src/main/scala/io/sphere/graphite/MongoLockReporter.scala
+++ src/main/scala/io/sphere/graphite/MongoLockReporter.scala
@@ -1,14 +1,28 @@
 package io.sphere.graphite
 
+import scala.language.reflectiveCalls
+
+import java.util.concurrent.TimeUnit
+
 import com.typesafe.config.{ConfigFactory, Config}
 
 object MongoLockReporter extends App {
-  val config = ConfigFactory.load()
-  val graphiteHost = config.getString("graphite.host")
-  val graphitePort = config.getInt("graphite.port")
-  val graphitePrefix = config.getString("graphite.prefix")
+  val config = new Conf(ConfigFactory.load())
 
+  println(s"Starting mongo locking reporting to ${config.graphite.host}:${config.graphite.port} with interval ${config.reportIntervalMs} ms...")
+}
 
-  println(s"Starting graphite reporting to $graphiteHost:$graphitePort...")
+class Conf(c: Config) {
+  val graphite = new {
+    val host = c.getString("graphite.host")
+    val port = c.getInt("graphite.port")
+    val graphitePrefix = c.getString("graphite.prefix")
+  }
 
+  val mongo = new {
+    val host = c.getString("mongo.host")
+    val port = c.getInt("mongo.port")
+  }
+
+  val reportIntervalMs = c.getDuration("reportInterval", TimeUnit.MILLISECONDS)
 }
