1d4b22afd4088165b03582b863737fef901a2b47
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2014-08-28 15:46:15 +0200

    Fixed prefix

--- src/main/scala/io/sphere/graphite/MongoLockReporter.scala
+++ src/main/scala/io/sphere/graphite/MongoLockReporter.scala
@@ -6,7 +6,7 @@ object MongoLockReporter extends App {
   val config = ConfigFactory.load()
   val graphiteHost = config.getString("graphite.host")
   val graphitePort = config.getInt("graphite.port")
-  val graphitePrefix = config.getInt("graphite.prefix")
+  val graphitePrefix = config.getString("graphite.prefix")
 
 
   println(s"Starting graphite reporting to $graphiteHost:$graphitePort...")
