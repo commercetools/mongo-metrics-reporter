91383bbbacc285f2bf40899ec5d8972e6916d793
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2014-08-29 19:12:51 +0200

    v1

--- src/main/scala/de/commercetools/graphite/MongoLockReporter.scala
+++ src/main/scala/de/commercetools/graphite/MongoLockReporter.scala
@@ -1,10 +1,15 @@
 package de.commercetools.graphite
 
+import java.io.{IOException, OutputStreamWriter}
+import java.net.Socket
+
+import language._
+
 import java.util.concurrent.TimeUnit
 import play.api.libs.iteratee.Iteratee
 import reactivemongo.api._
 import reactivemongo.api.collections.default.BSONCollection
-import reactivemongo.bson.{BSONValue, BSONDocument}
+import reactivemongo.bson._
 import reactivemongo.core.actors.Close
 import reactivemongo.utils.LazyLogger
 import rx.lang.scala.Observable
@@ -17,6 +22,7 @@ import com.typesafe.config.{ConfigFactory, Config}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
 
 object MongoLockReporter extends App {
   val config = new Conf(ConfigFactory.load())
@@ -25,27 +31,102 @@ object MongoLockReporter extends App {
   init()
 
   def init() = {
-    println(s"Starting mongo locking reporting to ${config.graphite.host}:${config.graphite.port} with interval ${config.reportIntervalMs} ms...")
+    logger.info(s"Starting mongo locking reporting to ${config.graphite.host}:${config.graphite.port} " +
+      s"from mongo ${config.mongo.url} " +
+      s"with interval ${config.reportIntervalMs}ms and prefix '${config.graphite.graphitePrefix}'")
 
     val driver = new MongoDriver
     val connection = driver.connection(config.mongo.url :: Nil)
 
     Observable.interval(config.reportIntervalMs milliseconds).flatMap(_ => Observable.from(getStats(connection))).subscribe(
-      onNext = sendToGraphite,
+      onNext = {
+        case Success(stats) => sendToGraphite(stats)
+        case Failure(error) => logger.error("Can't get stats from mongo!", error)
+      },
       onError = error => {
-        error.printStackTrace()
+        logger.error("Error in pipeline!", error)
         closeDriver(driver)
       },
-      onCompleted = () => closeDriver(driver)
+      onCompleted = () =>
+        closeDriver(driver)
     )
   }
 
-  def sendToGraphite(stats: Map[String, BSONValue]) = {
-    println("Sending!!!")
+  def extractStats(value: BSONValue): List[(String, Long)] = value match{
+    case BSONInteger(i) => List("" -> i.toLong)
+    case BSONDouble(d) => List("" -> d.toLong)
+    case BSONLong(l) => List("" -> l)
+    case BSONDocument(s) =>
+      s.toList.collect {case Success(pair) => pair}
+        .flatMap {case (key, v) => extractStats(v) map {case (sk, sv) => createKey(key, sk) -> sv}}
+    case _ => Nil
   }
 
+  def sendToGraphite(stats: Map[String, BSONValue]) = {
+    val all = stats.toList.flatMap {case (key, value) => extractStats(value) map {case (sk, sv) => createKey(key, sk) -> sv}}
+
+    try {
+      write(
+        new Socket(config.graphite.host, config.graphite.port),
+        all.map {case (key, value) => createKey(config.graphite.graphitePrefix, key) -> value})
+    } catch {
+      case e: IOException => logger.error("Error connecting to graphite!", e)
+    }
+  }
+
+  def write(sock: Socket, stats: List[(String, Long)]) = {
+    var writer: OutputStreamWriter = null
+
+    try {
+      val epoch = System.currentTimeMillis() / 1000
+
+      writer = new OutputStreamWriter(sock.getOutputStream)
+
+      try {
+        stats.foreach { case (key, value) =>
+          writer.write("%s %.2f %d\n".formatLocal(java.util.Locale.US, key, value.doubleValue, epoch))
+        }
+      } catch {
+        case e: IOException => logger.error("Error writing data to graphite!", e)
+      }
+
+      writer.flush()
+    } catch {
+      case e: Exception =>
+        logger.error("Error writing to Graphite!", e)
+
+        if (writer != null) {
+          try writer.flush() catch {
+            case ioe: IOException => logger.error("Error while flushing writer!", ioe)
+          }
+        }
+    } finally {
+      if (sock != null) {
+        try
+          sock.close()
+        catch {
+          case ioe: IOException => logger.error("Error while closing socket!", ioe)
+        }
+      }
+    }
+  }
+
+  def createKey(parts: String*) = parts
+    .filterNot(_.isEmpty)
+    .map {
+      case "." => "global_lock"
+      case name => camelToUnderscores(name.replaceAll(":", "_").replaceAll("/", ".").replaceAll(" ", "_"))
+    }
+    .mkString(".")
+
+  def camelToUnderscores(name: String) = "[A-Z\\d]".r.replaceAllIn(name, {m =>
+    "_" + m.group(0).toLowerCase()
+  })
+
   def getStats(connection: MongoConnection) =
-    connection("admin").command(Status().command).map(_.asInstanceOf[Map[String, BSONValue]])
+    connection("admin111").command(Status().command)
+      .map(s => Success(s.asInstanceOf[Map[String, BSONValue]]))
+      .recover {case error => Failure(error)}
 
   def closeDriver(driver: MongoDriver): Unit = {
     driver.connections.foreach(_.mongosystem ! Close)
