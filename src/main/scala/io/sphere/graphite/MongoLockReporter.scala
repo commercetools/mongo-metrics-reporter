0bac1794decaa0f7ea3a1b2b456110939386d0e2
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2014-08-29 11:38:10 +0200

    More progress

--- src/main/scala/io/sphere/graphite/MongoLockReporter.scala
+++ src/main/scala/io/sphere/graphite/MongoLockReporter.scala
@@ -1,27 +1,60 @@
 package io.sphere.graphite
 
-import scala.language.reflectiveCalls
-
 import java.util.concurrent.TimeUnit
+import play.api.libs.iteratee.Iteratee
+import reactivemongo.api._
+import reactivemongo.api.collections.default.BSONCollection
+import reactivemongo.bson.BSONDocument
+import reactivemongo.core.actors.Close
+import scala.concurrent.ExecutionContext.Implicits.global
+import reactivemongo.core.commands.Status
+import FutureHelper._
 
 import com.typesafe.config.{ConfigFactory, Config}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 object MongoLockReporter extends App {
   val config = new Conf(ConfigFactory.load())
 
-  println(s"Starting mongo locking reporting to ${config.graphite.host}:${config.graphite.port} with interval ${config.reportIntervalMs} ms...")
+  init()
+
+  def init() = {
+    println(s"Starting mongo locking reporting to ${config.graphite.host}:${config.graphite.port} with interval ${config.reportIntervalMs} ms...")
+
+    val driver = new MongoDriver
+    val connection = driver.connection(config.mongo.host :: Nil)
+
+    val adminDb = connection("admin")
+
+    println(adminDb.command(Status().command).awaitForever)
+    closeDriver(driver)
+  }
+
+  def closeDriver(driver: MongoDriver): Unit = {
+    driver.connections.foreach(_.mongosystem ! Close)
+    driver.close()
+  }
+}
+
+object FutureHelper {
+  implicit class FutureOps[T](f: Future[T]) {
+    def awaitForever = Await.result(f, Duration.Inf)
+  }
 }
 
 class Conf(c: Config) {
-  val graphite = new {
+  object graphite {
     val host = c.getString("graphite.host")
     val port = c.getInt("graphite.port")
     val graphitePrefix = c.getString("graphite.prefix")
   }
 
-  val mongo = new {
+  object mongo {
     val host = c.getString("mongo.host")
     val port = c.getInt("mongo.port")
+    def url = host + ":" + port
   }
 
   val reportIntervalMs = c.getDuration("reportInterval", TimeUnit.MILLISECONDS)
