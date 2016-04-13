cb835244e506ae097b7b54461eae89125626a11a
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2016-04-13 18:17:23 +0200

    Added support for multiple hosts

--- src/main/scala/de/commercetools/graphite/MongoMetricsReporter.scala
+++ src/main/scala/de/commercetools/graphite/MongoMetricsReporter.scala
@@ -3,8 +3,15 @@ package de.commercetools.graphite
 import com.typesafe.config.ConfigFactory
 import net.ceedubs.ficus.Ficus._
 
+import net.ceedubs.ficus.Ficus._
+import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+
+
 object MongoMetricsReporter extends App {
   val config = ConfigFactory.load()
+
+  val mongoConfig = config.as[MongoConfig]("mongo")
+  println(mongoConfig.urls)
 
   config.as[String]("reporter") match {
     case "influxDb" => new MongoInfluxDBReporter(config)
@@ -13,6 +20,12 @@ object MongoMetricsReporter extends App {
   }
 }
 
-case class MongoConfig(host: String, port: Int, databaseId: String) {
+case class MongoConfig(hosts: Option[String], mongosHost: Option[String], mongosPort: Option[Int], host: String, port: Int) {
   lazy val url = host + ":" + port
+  lazy val mongosUrl = for {h ← mongosHost; p ← mongosPort} yield h + ":" + p
+
+  lazy val parsedHosts =
+    hosts.fold(Nil: List[String])(_.split("\\s*,\\s*").toList)
+
+  lazy val urls = if (parsedHosts.nonEmpty) parsedHosts else List(url)
 }
